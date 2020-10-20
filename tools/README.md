3a6638aeaca4ae431bf7ed80d28e70409b450f2c
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2020-10-20 16:30:04 +0200

    Added MongoDB installer script for Windows

--- tools/README.md
+++ tools/README.md
@@ -13,9 +13,35 @@ sudo ./install_mongodb.sh
 
 ### Windows
 
-TODO
+> Note: When prompted by the installer choose to install MongoDB as a service
+
+```bash
+.\install_mongodb.bat
+```
 
 ## 🏃‍♂️ Run
 
 TODO
 
+
+
+## ❌Uninstall
+
+### Linux + WSL2
+
+```bash
+sudo apt-get remove mongodb
+```
+
+### Windows
+
+```bash
+sc.exe delete MongoDB
+```
+
+
+
+## 📕 Sources
+
+* https://docs.mongodb.com/manual/tutorial/install-mongodb-on-windows/
+* https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/
---
+++ tools/install_mongodb.bat
@@ -0,0 +1,18 @@
+@ECHO OFF
+
+:: Set Donwload path
+set mypath=%~dp0
+if not exist temp (mkdir temp)
+set file=temp\installer.msi
+set downloadpath=%mypath%%file%
+:: Set URL
+set url="https://fastdl.mongodb.org/windows/mongodb-windows-x86_64-4.4.1-signed.msi"
+:: Download installer
+echo Downloading installer...
+echo This can take a while...
+powershell -command "(New-Object Net.WebClient).DownloadFile(\"%url%\", \"%downloadpath%\")"
+echo Dowload succeeded
+:: Run installer
+msiexec /i %downloadpath%
+:: Remove installer
+RMDIR temp /S /Q
