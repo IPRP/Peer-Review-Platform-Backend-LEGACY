db901a1810ad909b7a4c7e421b06de46c91c84df
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2020-10-20 19:07:02 +0200

    Program will now try to start MongoDB before Spring Boot startup

--- src/main/kotlin/com/iprp/backend/BackendApplication.kt
+++ src/main/kotlin/com/iprp/backend/BackendApplication.kt
@@ -1,5 +1,6 @@
 package com.iprp.backend
 
+import com.iprp.backend.tools.MongoUtilities
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,9 @@ import org.springframework.boot.runApplication
 class BackendApplication
 
 fun main(args: Array<String>) {
+    // Start MongoDB via start scripts
+    // Works on Windows + Ubuntu
+    MongoUtilities.start()
+    // Start Spring
     runApplication<BackendApplication>(*args)
 }
