5c72bce24f0300ac526489567a8995d579106c43
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2020-10-21 18:43:20 +0200

    Added custom Authentication Provider Also disabled Spring Security for development now

---
+++ src/main/kotlin/com/iprp/backend/api/RestTemplate.java
@@ -0,0 +1,20 @@
+package com.iprp.backend.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Simple Rest Controller for testing.
+ *
+ * @author Kacper Urbaniec
+ * @version 2020-10-21
+ */
+
+@RestController
+public class RestTemplate {
+
+    @GetMapping("/")
+    public String sayHi() {
+        return "Hi!";
+    }
+}
