8e9ab282e5abd8b7de1c18e513e6bcbd8c248acb
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2020-11-26 19:42:59 +0100

    Added method definitions in DataManagement Also defined some TODOs!

--- src/main/kotlin/com/iprp/backend/DataManagement.kt
+++ src/main/kotlin/com/iprp/backend/DataManagement.kt
@@ -27,6 +27,8 @@ class DataManagement {
     @Autowired
     lateinit var repo: WrapperRepository
 
+    // TODO read write lock ReentrantReadWriteLock
+
     // Scheduled task
     // See: https://spring.io/guides/gs/scheduling-tasks/
     @Scheduled(fixedRate = 50000)
@@ -38,49 +40,53 @@ class DataManagement {
      * Workshop
      */
 
-    fun findAllWorkshops(personId: String): Map<String, List<Map<String, String>>> {
+    fun findAllWorkshops(personId: String): Map<String, Any> {
         val workshops = repo.findAllWorkshops(personId)
         val workshopList = mutableListOf<Map<String, String>>()
         for (workshop in workshops) {
             workshopList.add(mapOf("id" to workshop.id, "title" to workshop.title))
         }
-        return Collections.singletonMap("workshops", workshopList)
+        return mapOf("workshops" to workshopList)
     }
 
     fun addWorkshop(
         teacherIds: List<String>, studentIds: List<String>,
         title: String, content: String, anonymous: Boolean,
         end: LocalDateTime, criteria: List<Map<String, String>>
-    ) {
-        // Create workshop
-        //val teachers = repo.findAllTeachersByIdIn(teacherIds) as MutableList<Teacher>
-        val students = repo.findAllStudentsByIdIn(studentIds) as MutableList<Student>
-        val roundEnd = LocalDateTime.now().plusWeeks(2)
-        val rounds = mutableListOf<String>()
-        val grades = mutableListOf<String>()
+    ): Map<String, Any>  {
+        try {
+            // Create workshop
+            val students = repo.findAllStudentsByIdIn(studentIds) as MutableList<Student>
+            val roundEnd = LocalDateTime.now().plusWeeks(2)
+            val rounds = mutableListOf<String>()
+            val grades = mutableListOf<String>()
 
-        var criteriaType = ReviewCriteria.fromList(criteria)
-        criteriaType = repo.saveReviewCriteria(criteriaType)
+            var criteriaType = ReviewCriteria.fromList(criteria)
+            criteriaType = repo.saveReviewCriteria(criteriaType)
 
-        var workshop = Workshop(
-            title, content, end, roundEnd, anonymous, studentIds as MutableList<String>,
-            teacherIds as MutableList<String>, criteriaType.id, rounds, grades
-        )
-        workshop = repo.saveWorkshop(workshop)
+            var workshop = Workshop(
+                    title, content, end, roundEnd, anonymous, studentIds as MutableList<String>,
+                    teacherIds as MutableList<String>, criteriaType.id, rounds, grades
+            )
+            workshop = repo.saveWorkshop(workshop)
 
-        // Add grade structure for every student
-        for(student in students) {
-            var grade = GradeCollection(null, mutableListOf(), student.id, workshop.id)
-            grade = repo.saveGradeCollection(grade)
-            grades.add(grade.id)
+            // Add grade structure for every student
+            for (student in students) {
+                var grade = GradeCollection(null, mutableListOf(), student.id, workshop.id)
+                grade = repo.saveGradeCollection(grade)
+                grades.add(grade.id)
+            }
+
+            // Update workshop in order to add grades
+            workshop = repo.saveWorkshop(workshop)
+
+            // Add first submission round
+            startRound(workshop, cachedStudents = students)
+
+            return mapOf("ok" to true)
+        } catch (ex: Exception) {
+            return mapOf("ok" to false)
         }
-
-        // Update workshop in order to add grades
-        workshop = repo.saveWorkshop(workshop)
-
-        // Add first submission round
-        startRound(workshop, cachedStudents = students)
-
     }
 
     private fun startRound(workshop: Workshop, cachedStudents: MutableList<Student>? = null) {
@@ -103,18 +109,18 @@ class DataManagement {
             }
 
         }
-        val submissionRound = SubmissionRound(workshop.roundEnd, workshop.id, submissions, grades)
-        repo.saveSubmissionRound(submissionRound)
+        var submissionRound = SubmissionRound(workshop.roundEnd, workshop.id, submissions, grades)
+        submissionRound = repo.saveSubmissionRound(submissionRound)
+        workshop.rounds.add(submissionRound.id)
+        repo.saveWorkshop(workshop)
     }
 
-
-    fun addStudentToWorkshop(studentId: String) {
-        throw NotImplementedError()
-        // inversion of remove student from workshop
-    }
-
-    fun removeStudentFromWorkshop(studentId: String) {
-        throw NotImplementedError()
+    fun updateWorkshop(
+        workshopId: String, teacherIds: List<String>, studentIds: List<String>,
+        title: String, content: String, anonymous: Boolean,
+        end: LocalDateTime, criteria: List<Map<String, String>>
+    ): Map<String, Any> {
+        // TODO do not kick out teacher out of course
 
         // Lock process for this id
         // See: https://stackoverflow.com/a/13957003/12347616
@@ -128,7 +134,16 @@ class DataManagement {
         // remove student from submission round
 
         //remove student from grades
+
+        throw NotImplementedError()
     }
+
+    fun deleteWorkshop(workshopId: String): Map<String, Any> {
+        throw NotImplementedError()
+    }
+
+
+
 
     /**
      * Person
